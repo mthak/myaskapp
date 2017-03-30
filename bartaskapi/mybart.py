a99dccc4c3f7b246e99eb7ee4322427f7df8f751
root <root@ip-172-31-8-114.us-west-1.compute.internal>
2017-03-30 23:08:34 +0000

    Add additional changes with more help messages.remove ',' from the response. create response string in the last

--- bartaskapi/mybart.py
+++ bartaskapi/mybart.py
@@ -12,6 +12,7 @@ def validate_name(stn,stnlist):
    stfind = False
    for items in stnname:
        # Stattion name can be a substring of actual station name
+       print "items ins " , items
        if stn.lower() in items.lower():
           stfind = True
           logging.info("found station yeah ! %s", stfind)
@@ -42,14 +43,15 @@ def validate_stn(stnname,stnlist):
 def getrouteInfo(source,dest):
     stns = requests.get("http://bart.crudworks.org/api/stations")
     stnlist = json.loads(stns.content)
+    print "stattion list " , stnlist
     source = validate_stn(source,stnlist)
     if not source:
-       return [" Invalid Source Station , Please pick a valid station"]
+       return [" You have specified an invalid source stattion , Please ask help to get list of stations" ]
     dest = validate_stn(dest,stnlist)
     print "destination is ", dest
     if not dest:
        print "dest not found"
-       return ["Invalid destination"]
+       return ["The destination station you requested is not correct. Please ask help to get lis of all bart stations"]
     print "source and etination are ", source,dest
     allroutes = requests.get("http://api.bart.gov/api/route.aspx?cmd=routes&key="+key)
     routesjson = xmltodict.parse(allroutes.content)
@@ -58,9 +60,6 @@ def getrouteInfo(source,dest):
     result = []
     for route in myroutes:
         print json.dumps(route)
-        # route abbr is like WARM-DALY
-        #if route['abbr'].split('-')[0] == source:
-           # get the route id and append to list
         print "route id is ", route['number']
         routeid.append(route['number'])
     for rid in routeid:
@@ -69,20 +68,33 @@ def getrouteInfo(source,dest):
         stationlist = routeinfo['root']['routes']['route']['config']['station']
         print " station list " , stationlist
         if dest in stationlist and source in stationlist and stationlist.index(source) < stationlist.index(dest):
-           print "found you ..."
            finaldest = routeinfo['root']['routes']['route']['destination']
            if finaldest != source:
-              print "heelo there ",source,finaldest
-              print " my station list" , stationlist
+              logging.debug(" my station list %s" , stationlist)
               print "my route number " , rid
               mydata = getSchedule(source,finaldest)
               print " mydata is ", mydata
               if mydata:
-                 result.append(''.join(mydata))
-    print result
+                 result.extend(mydata)
+    print " data is ",result
+    # remove duplicated and dump it back in list
+    # set does not order data back in same manner so do a sort later for the list
     if not result:
        result = ["No train available from Source to Destination"]
-    return set(result)   
+    # onlu if we get result as list of strings we shall add next train string
+    if all(isinstance(item, int) for item in result):
+        result = list(set(result))
+        result.sort(key=int)
+        result = [ "next train in "+str(items) +"  minutes      " for items in result ]
+    print "result is " , result  
+    # alexa does not like lists as it speaks , as comma in response so convert it to string
+    spaces = ' '*50
+    if len(result) > 5 :
+       result = spaces.join(result[:5])
+    else:
+        result =spaces.join(result)
+    print "result is " , result  
+    return result 
 
 def getSchedule(source,dest):
     stns = requests.get("http://bart.crudworks.org/api/stations")     
@@ -90,11 +102,11 @@ def getSchedule(source,dest):
     print "source and destination is " , source,dest
     source = validate_stn(source,stnlist)
     if not source:
-       return [" Invalid Source Station , Please pick a valid station"]
+       return [" Invalid Source Station , Please pick a valid station. Ask help for list of all stations"]
     dest = validate_stn(dest,stnlist)
     print "destination is ", dest
     if not dest:
-       return ["Invalid destination"]
+       return ["The destination station in not correct. Please ask help for list of all stations"]
     print "station validate" , source
     result = []
     newdata = {}
@@ -106,7 +118,7 @@ def getSchedule(source,dest):
        mydict = xmltodict.parse(data)
        print json.dumps(mydict,indent=4)
     else:
-        result = ["Sorry Bart API is not reachable"]
+        result = ["Sorry Could not retrieve bart information at this time. Please try later"]
         return result
     try:
        stationinfo = mydict['root']['station']
@@ -122,19 +134,19 @@ def getSchedule(source,dest):
     for alldestinations in destinationinfo:
         if alldestinations['abbreviation'] == dest:
            found = True
-           alltimes = [ str(data['minutes']) for data in alldestinations['estimate'] ]
-           print "time is " , alltimes
+           if type (alldestinations['estimate']) != list:
+              alltimes = [ alldestinations['estimate']['minutes'] ]
+           else:
+               alltimes = [ data['minutes'] for data in alldestinations['estimate'] ]
            for time in alltimes:
-              if "Leaving" not in time:
-                  sttime = "next train in " + time + "minutes"
-              else: 
-                  sttime = "next train Leaving now"
-              result.append(sttime)
+              if "Leaving" in time:
+                  time = 0
+              result.append(int(time))
     if not found:
        result = None
        #result = ["No train available from Source to Destination"]
     return  result
 
 #my test playground 
-#ret = getrouteInfo("Coliseum","Bay fair")
+#ret = getrouteInfo("Dublin","Fremont")
 #print ret
